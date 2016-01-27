c1d638143f70e0b28f2ba2c4ac487c0296aee0d4
Schuyler Klaassen and Will Murphy <schuyler.m.k@gmail.com>
2016-01-26 16:45:01 -0800

    add final project yay

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 
-var BankAccount = function(name, initialDeposit) {
+function BankAccount(name, initialDeposit) {
     this.name = name;
     this.balance = initialDeposit;
+}
+BankAccount.prototype.nameAndBalance = function() {
+  return "Account Holder - " + this.name + ": Current Balance = $" + this.balance;
+}
+
+BankAccount.prototype.deposit = function(cash) {
+  this.balance += cash;
+}
+
+BankAccount.prototype.withdrawal = function(cash) {
+  this.balance -= cash;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,21 @@
 describe ("BankAccount", function() {
-  it("it will create an account with the following information", function() {
-    var myAccount = new BankAccount("name",25);
-    expect(myAccount.name).to.equal("name");
+  it("will create an account with the following information", function() {
+    var myAccount = new BankAccount("Bucky",25);
+    expect(myAccount.name).to.equal("Bucky");
     expect(myAccount.balance).to.equal(25);
   });
-  it(it will create)
+  it("will create a prototype concatenating Name and Balance", function() {
+    var myAccount = new BankAccount("Bucky",25);
+    expect(myAccount.nameAndBalance()).to.equal("Account Holder - Bucky: Current Balance = $25");
+  });
+  it("will create deposit method to add to the balance", function() {
+    var myAccount = new BankAccount("Bucky",25);
+    myAccount.deposit(10);
+    expect(myAccount.balance).to.equal(35);
+  });
+  it("will create withdrawal method to subtract from the balance", function() {
+    var myAccount = new BankAccount("Bucky",25);
+    myAccount.withdrawal(10);
+    expect(myAccount.balance).to.equal(15);
+  });
 });
